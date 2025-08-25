e745ea0bdbc1dbceb710e0553c96c2082d806ce2
jgalaber <jgalaberdev@gmail.com>
2025-08-25 02:11:16 +0200

    refactor: update packages for middlewares

--- test/middlewares/node-express-v5-vue/index.js
+++ test/middlewares/node-express-v5-vue/index.js
@@ -1,18 +1,14 @@
-import express from "express";
-import Prerender, { Adapters } from "prerender-agnostic";
+import express from 'express';
+import Prerender, { Adapters } from 'prerender-agnostic';
 
 const PORT = 8002;
 const app = express();
 
 app.use(
-  Adapters.expressPrerender(
-    new Prerender().set("prerenderServiceUrl", "http://localhost:3000")
-  )
+  Adapters.expressPrerender(new Prerender().set('prerenderServiceUrl', 'http://localhost:3000')),
 );
-app.use(express.static("../public"));
+app.use(express.static('../public'));
 
 app.listen(PORT, () => {
-  console.log(
-    `Prerender Vue.js example app is listening at http://localhost:${PORT}`
-  );
+  console.log(`Prerender Vue.js example app is listening at http://localhost:${PORT}`);
 });
